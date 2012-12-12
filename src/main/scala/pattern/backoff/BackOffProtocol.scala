c43bb01eaa564f6e807adabe8e0c0690dab62fa7
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2012-12-12 16:42:43 +0100

    refined some comments

--- src/main/scala/pattern/backoff/BackOffProtocol.scala
+++ src/main/scala/pattern/backoff/BackOffProtocol.scala
@@ -7,21 +7,21 @@ import akka.actor.ActorRef
  */
 object BackOffProtocol {
   /**
-   * The message type that is used in the domain. The BackOffSender sends this type of message
-   * to its child, which performs some dangerous operation on it.
+   * The message type that is used in the domain. The BackOffSender forwards this type of message
+   * to its dangerous child, which performs some dangerous operation on it.
    */
   case class Msg(id: Long, data: String)
 
   /**
-   * Used between the supervisor and the child producer to track the original sender of the Msg message.
+   * Used between the supervisor (the BackOffSender) and the child dangerous actor to track the original sender of the Msg message.
    * When a message fails for a while and succeeds after a number of retries, the original sender of the
    * Msg can be responded to.
    */
   case class TrackedMsg(msg: Msg, sender: ActorRef)
 
   /**
-   * Indicates that the producer successfully sent a message to the endpoint.
-   * When the BackOffSender receives this message it resets the back off algorithm
+   * The dangerous actor needs to send this message to its supervisor (the BackOffSender) when it has successfully
+   * processed the message. When the BackOffSender receives this message it resets the back off algorithm.
    */
   case class Sent(id: Long)
 }
